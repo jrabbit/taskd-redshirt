aa8edb7aebe496b15416e2c9a1efd1b07dcad4ad
amelia sabine <amelia.sabine@riseup.net>
2016-07-19 13:08:04 -0700

    give mock_open read data to pass to install_cert()

--- redshirt_test.py
+++ redshirt_test.py
@@ -1,7 +1,11 @@
 import unittest
 import subprocess
+import psutil
 
-import mock
+try:
+    from unittest.mock import patch, mock_open, NonCallableMagicMock
+except ImportError:
+    from mock import patch, mock_open, NonCallableMagicMock
 
 from redshirt import *
 from redshirt import __version__
@@ -11,7 +15,7 @@ class TestMetaRoutes(unittest.TestCase):
     def test_version(self):
         self.assertEqual(version(), __version__)
 
-    @mock.patch('socket.gethostname')
+    @patch('socket.gethostname')
     def test_index(self, patched_gethostname):
         host = "hal"
         patched_gethostname.return_value = host
@@ -25,36 +29,32 @@ class TestMetaRoutes(unittest.TestCase):
 class TestUserCreateStory(unittest.TestCase):
     org = "Testusers"
     user = "Timmy"
-    @mock.patch("redshirt.check_output")
+    @patch("redshirt.check_output")
     def test_add_user(self, patched_check_output):
         patched_check_output.return_value = "New user key: 570472ff-43b5-43f9-8544-e3b2fa5cc6f2\nCreated user '{}' for organization '{}'\n".format(self.user, self.org)
         self.assertEqual(add_user(self.org, self.user), "570472ff-43b5-43f9-8544-e3b2fa5cc6f2")
         patched_check_output.assert_called_with(["taskd", "add", "user", self.org, self.user])
 
-    @mock.patch("redshirt.check_output")
+    @patch("redshirt.check_output")
     def test_rm_user(self, patched_check_output):
         patched_check_output.return_value = ""
         self.assertEqual(remove_user(self.user, self.org), "OK")
         patched_check_output.assert_called_with(["taskd", "remove", "user", self.org, self.user])
 
-    @mock.patch("redshirt.check_output")
+    @patch("redshirt.check_output")
     def test_add_org(self, patched_check_output):
         new_org = "TeamRocket"
         self.assertEqual(add_org(new_org), "OK")
         patched_check_output.assert_called_with(["taskd", "add", "org", new_org])
     
-    @mock.patch("redshirt.check_output")
+    @patch("redshirt.check_output")
     def test_rm_org(self, patched_check_output):
         new_org = "TeamRocket"
         self.assertEqual(rm_org(new_org), "OK")
         patched_check_output.assert_called_with(["taskd", "remove", "org", new_org])
 
-
-        
-
-
 class TestTaskInfo(unittest.TestCase):
-    @mock.patch('redshirt.check_output')
+    @patch('redshirt.check_output')
     def test_version(self, patched_check_output):
         patched_check_output.return_value = "\n\x1b[1mtaskd 1.2.0\x1b[0m e2d145b built for linux\nCopyright (C) 2010 - 2016 G\xc3\xb6teborg Bit Factory.\n\nTaskd may be copied only under the terms of the MIT license, which may be found in the taskd source kit.\nDocumentation for taskd can be found using 'man taskd' or at http://taskwarrior.org\n\n"
         out = get_version()
@@ -62,21 +62,20 @@ class TestTaskInfo(unittest.TestCase):
         self.assertEqual(out, expected)
         patched_check_output.assert_called_with(["taskd", "-v"])
 
-    @mock.patch('redshirt._get_proc')
+    @patch('redshirt._get_proc')
     def test_health_check(self, patched_get_proc):
         # patched_process_iter.side_effect = {"taskd"
-        patched_get_proc.return_value = [Process()]
+        patched_get_proc.return_value = [psutil.Process()]
         self.assertEqual(health_check(), {"status": ""})
         patched_process_iter.assert_called_with()
 
 class TestCerts(unittest.TestCase):
 
     # https://stackoverflow.com/questions/37265706/how-can-i-use-mock-open-with-a-python-unittest-decorator
-    @mock.patch('__main__.open',new_callable=mock.mock_open)
-    @mock.patch('bottle.request')
-    def test_install_cert(self, patched_request, mocked_open):
-        self.assertEqual(install_cert(), "OK")
-
+    @patch('redshirt.request')
+    def test_install_cert(self, patched_request):
+        with patch('redshirt.open', mock_open(read_data="this is a test cert"), create=True):
+            self.assertEqual(install_cert(), "OK")
 
 class IntegrationTest(unittest.TestCase):
     pass
